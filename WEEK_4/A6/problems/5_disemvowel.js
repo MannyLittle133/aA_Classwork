2d616300079572b4f498d9bf4a623f95100a5e1e
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-08-30 19:10:03 -0400

    new day 2

--- WEEK_4/A6/problems/5_disemvowel.js
+++ WEEK_4/A6/problems/5_disemvowel.js
@@ -10,16 +10,32 @@ disemvowel('hello world'); // => 'hll wrld'
 *******************************************************************************/
 
 function disemvowel(string) {
- let vowels = 'AEIOUaeiou'
- let str = string.split('')
- let arr = []
 
- for (let i = 0; i < string.length; i++) {
+    let vowels = {
+      'a': true,
+      'e': true,
+      'i': true,
+      'o': true,
+      'u': true
+    }
   
+    let result = "";
+  
+    for (let i = 0; i < string.length; i++) {
+      let letter = string[i].toLowerCase();
 
- }
+      if (!vowels[letter]) {
 
-}
+        result += string[i];
+        
+      }
+    }
+    return result;
+  }
 
+
+console.log(disemvowel('bootcamp')); // => 'btcmp'
+console.log(disemvowel('PREP')); // => 'PRP'
+console.log(disemvowel('hello world')); // => 'hll wrld'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = disemvowel;
